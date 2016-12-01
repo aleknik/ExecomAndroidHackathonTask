a06244697558f45a6eaa9aad400a4a91cee75c2f
Aleksandar Nikolic <nikolic95@gmail.com>
2016-12-01 21:10:52 +0100

    Inital commit

---
+++ app/src/main/java/eu/execom/todolistgrouptwo/api/RestApi.java
@@ -0,0 +1,34 @@
+package eu.execom.todolistgrouptwo.api;
+
+import org.androidannotations.rest.spring.annotations.Body;
+import org.androidannotations.rest.spring.annotations.Get;
+import org.androidannotations.rest.spring.annotations.Header;
+import org.androidannotations.rest.spring.annotations.Post;
+import org.androidannotations.rest.spring.annotations.Rest;
+import org.springframework.http.converter.FormHttpMessageConverter;
+import org.springframework.http.converter.json.GsonHttpMessageConverter;
+import org.springframework.util.LinkedMultiValueMap;
+
+import java.util.List;
+
+import eu.execom.todolistgrouptwo.api.interceptor.AuthenticationInterceptor;
+import eu.execom.todolistgrouptwo.constant.ApiConstants;
+import eu.execom.todolistgrouptwo.model.Task;
+import eu.execom.todolistgrouptwo.model.dto.TokenContainerDTO;
+
+
+@Rest(rootUrl = ApiConstants.ROOT_URL, converters = {GsonHttpMessageConverter.class,
+        FormHttpMessageConverter.class},
+        interceptors = AuthenticationInterceptor.class)
+public interface RestApi {
+
+    @Header(name = "Content-Type", value = "application/x-www-form-urlencoded")
+    @Post(value = ApiConstants.LOGIN_PATH)
+    TokenContainerDTO login(@Body LinkedMultiValueMap<String, String> accountInfo);
+
+    @Get(value = ApiConstants.TASK_PATH)
+    List<Task> getAllTasks();
+
+    @Post(value = ApiConstants.TASK_PATH)
+    Task createTask(@Body Task task);
+}
